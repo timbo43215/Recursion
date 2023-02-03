c8904926ec25f1532f8e9ff3ccf2326ebac0a553
Tim Stack <tstack43215@yahoo.com>
2023-02-03 13:17:55 -0600

    Initial Commit

---
+++ Recursion/RecursionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecursionApp.swift
+//  Recursion
+//
+//  Created by IIT PHYS 440 on 2/3/23.
+//
+
+import SwiftUI
+
+@main
+struct RecursionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
